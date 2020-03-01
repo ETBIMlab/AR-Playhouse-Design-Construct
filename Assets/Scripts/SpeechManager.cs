c97542d89c6252fbb8c1db7dcf4a8b1f34d26182
Abrar Akhand <Jahin345@gmail.com>
2020-03-01 12:25:48 -0700

    Added Basic Voice Command (SpeechManger.cs and TestCommands.cs)
    SpeechManager.cs adds keywords to a dictionary and listens for keywords/commands.
    Only command as of now is "Create Cube" which spawns a pink cube in front of the player (implementation in TestCommands.cs)
    TestCommands.cs contains the methods that will be called by SpeechManger script
    To test this, I added the 2 scripts to the Main Camera in the "Main" scene
    Tested in the unity testing window, not emulator but should work

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        // global command
+        keywords.Add("Create Cube", () =>   // added the "Create Cube" command to dictionary of commands (keywords)
+        {
+            //Debug.Log("Create Cube");
+            // Call the OnReset method on every descendant object.
+            this.BroadcastMessage("CreateCube");    // when this keyword is invoked, this will be broadcasted, calling the method CreateCube
+        });
+
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and START recognizing!!!!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    // important
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();     // if speech command recognized, then invoke the action
+        }
+    }
+}
---
+++ Assets/Scripts/TestCommands.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCommands : MonoBehaviour
+{
+
+    // for attaching a cube object in the editor
+    [SerializeField] GameObject testCube;
+
+    void Start()
+    {
+        
+    }
+
+    // Called by SpeechManager when the user says the "Create Cube" command (creates a pink cube in front of player)
+    void CreateCube()
+    {
+        Debug.Log("Creating Pink Cube!");
+
+        int distFromCamera = 3;
+
+        Instantiate(testCube, transform.position + transform.forward * distFromCamera, Quaternion.identity);    // creates a cube in front of camera
+    }
+
+   
+}
