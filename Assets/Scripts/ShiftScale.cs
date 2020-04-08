2d03fe1828272820f0e4fefc236b2f36b1cd7f71
parker@squaredev.com <parker@squaredev.com>
2020-04-07 20:52:48 -0700

    fix scaling script, add floor

--- Assets/Scripts/ShiftScale.cs
+++ Assets/Scripts/ShiftScale.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class ShiftScale : MonoBehaviour
 {
     private GameObject cube; 
-    private Vector scaleChange, positionChange; 
+    private Vector3 scaleChange, positionChange; 
     // Use this for initialization
     void Awake()
     {
         //create cube at origin
         cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = new Vector(0, 0, 0);
+        cube.transform.position = new Vector3(0, 0, 0);
 
         //create plane and adjust down by 0.5
         GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        plane.transform.position = new Vector(0, -0.5f, 0);
-
-        scaleChange = new Vector(-0.01f, -0.01f, -0.01f);
-        positionChange = new Vector(0.0f, -0.005f, 0.0f);
+        plane.transform.position = new Vector3(0, -0.5f, 0);
+        
+        scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+        positionChange = new Vector3(0.0f, -0.005f, 0.0f);
     }
     void Update()
     {
