75f642f5c69003a4bbe66b7617274929e596c5af
Andrei-Iosifescu <aiosifes@asu.edu>
2020-10-27 23:55:25 -0700

    Robust object ordering

--- Assets/Scripts/ObjectOrderer.cs
+++ Assets/Scripts/ObjectOrderer.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
 using UnityEngine.Windows.Speech;
 
 public class ObjectOrderer : MonoBehaviour
@@ -18,6 +19,9 @@ public class ObjectOrderer : MonoBehaviour
     KeywordRecognizer keywordRecognizer = null;
     List<string> keywords = new List<string>();
     public OrderableObj[] orderableObjs;
+    List<string> numlist = new List<string>()  {
+                        "zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"};
+
 
 
     // Start is called before the first frame update
@@ -26,6 +30,11 @@ public class ObjectOrderer : MonoBehaviour
         for(int i = 0; i < orderableObjs.Length; i++)
         {
             keywords.Add("Order " + orderableObjs[i].name);
+            keywords.Add("Order a dozen " + orderableObjs[i].name+"s");
+            for (int j = 0; j < numlist.Count; j++)
+            {
+                keywords.Add("Order "+ numlist[j] + " " + orderableObjs[i].name+"s");
+            }
         }
 
         // Tell the KeywordRecognizer about our keywords.
@@ -39,11 +48,36 @@ public class ObjectOrderer : MonoBehaviour
     // important
     private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        for(int i = 0; i < orderableObjs.Length; i++)
+        string temp = args.text.Substring(args.text.LastIndexOf(" ") + 1);
+        temp = temp.Substring(0, temp.Length-1);
+        for (int i = 0; i < orderableObjs.Length; i++)
         {
-            if(args.text.Substring(6) == orderableObjs[i].name)
+
+            if (args.text.Substring(6) == orderableObjs[i].name)
             {
                 AddObjectToScene(orderableObjs[i].obj);
+                return;
+            }
+            else if(temp == orderableObjs[i].name)
+            {
+                string h = args.text.Substring(6);
+                h = h.Substring(0,h.IndexOf(" "));
+                Debug.Log(h);
+                int hold = numlist.IndexOf(h);
+                if (hold != -1)
+                {
+                    for (int j = 0; j < hold; j++)
+                    {
+                        AddObjectToScene(orderableObjs[i].obj);
+                    }
+                }
+                else if(h.Equals("a"))
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        AddObjectToScene(orderableObjs[i].obj);
+                    }
+                }
                 return;
             }
         }
