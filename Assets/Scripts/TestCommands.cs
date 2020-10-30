aa6d82516be7957ad845e23c602e46d77720b590
Kenny Le <42878549+LeKenny7@users.noreply.github.com>
2020-10-30 01:57:00 -0700

    Revert "Merge branch 'ObjectCreation' into KennysBranch"
    This reverts commit d8240a59004a8c70b9508ce6bbe9b967c7c0ffc9, reversing
    changes made to c03f231b691a54fb44fe3c3dbd339f16bb7c4a33.

--- Assets/Scripts/TestCommands.cs
+++ Assets/Scripts/TestCommands.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class TestCommands : MonoBehaviour
 {
-    //Audio Source Object to play a audio clip when spawning an object
-    private AudioSource audioSource;
 
     // for attaching a cube object in the editor
     public GameObject twoByFour;
@@ -14,11 +12,9 @@ public class TestCommands : MonoBehaviour
     public GameObject screw;
     public GameObject playhouseEnvironment;
 
-    public AudioClip spawnClip;//clip to be played
-
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();//get the audio source from the object for the audio to be played
+        
     }
 
     // Called by SpeechManager when the user says the "Create Cube" command (creates a pink cube in front of player)
@@ -29,29 +25,21 @@ public class TestCommands : MonoBehaviour
         int distFromCamera = 3;
 
         Instantiate(twoByFour, transform.position + transform.forward * distFromCamera, Quaternion.identity);    // creates a cube in front of camera
-
-        audioSource.PlayOneShot(spawnClip, 1F);//play audio clip
     }
     void OrderLadder()
     {
         int distFromCamera = 3;
         Instantiate(ladder, new Vector3(56,-20,150), Quaternion.identity);
-
-        audioSource.PlayOneShot(spawnClip, 1F);//play audio clip
     }
     void OrderSlide()
     {
         int distFromCamera = 3;
         Instantiate(slide, new Vector3(0, -20, 150), Quaternion.identity);
-
-        audioSource.PlayOneShot(spawnClip, 1F);//play audio clip
     }
     void OrderScrew()
     {
         int distFromCamera = 1;
         Instantiate(screw, new Vector3(48, -29, 101), Quaternion.identity);
-
-        audioSource.PlayOneShot(spawnClip, 1F);//play audio clip
     }
 
     void ChangeSize()
