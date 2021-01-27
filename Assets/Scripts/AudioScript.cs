eb0e5a48dda098b3756a42f1b933e77c4f1c4303
Albert De La Cruz <adelac10@asu.edu>
2021-01-27 14:46:50 -0700

    Added all of Albert's work to the Fall 2020 branch

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+
+//script made by Albert De La Cruz
+//child of Mixed Reality Pointer Handler as a base 
+public class AudioScript : MonoBehaviour, IMixedRealityPointerHandler
+{
+
+    private AudioSource theAudio1;
+    public AudioClip grabClip;
+    public AudioClip releaseClip;
+    public AudioClip dragClip;
+    public AudioClip clickClip;
+    public float volume = 1F;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio1 = GetComponent<AudioSource>();//gets the audio file to be played on grab
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //plays audio file when the object is grabbed by the user
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        theAudio1.PlayOneShot(grabClip, volume);
+    }
+
+    //plays audio file when object is let go by the user
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        theAudio1.PlayOneShot(releaseClip, volume);
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        theAudio1.PlayOneShot(dragClip, volume);
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        theAudio1.PlayOneShot(clickClip, volume);
+    }
+
+   
+}
--- Assets/Scripts/TestCommands.cs
+++ Assets/Scripts/TestCommands.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class TestCommands : MonoBehaviour
 {
+    //Audio Source Object to play a audio clip when spawning an object
+    private AudioSource audioSource;
+    public float volume = 1F;
+    public AudioClip spawnClip;
+    public string audioTarget;
+    private GameObject gameLog;
 
     // for attaching a cube object in the editor
     public GameObject twoByFour;
@@ -14,7 +20,9 @@ public class TestCommands : MonoBehaviour
 
     void Start()
     {
-        
+        //audioSource = GetComponent<AudioSource>();//get the audio source from the object for the audio to be played
+        gameLog = GameObject.Find(audioTarget);
+        audioSource = gameLog.GetComponent<AudioSource>();
     }
 
     // Called by SpeechManager when the user says the "Create Cube" command (creates a pink cube in front of player)
@@ -25,21 +33,30 @@ public class TestCommands : MonoBehaviour
         int distFromCamera = 3;
 
         Instantiate(twoByFour, transform.position + transform.forward * distFromCamera, Quaternion.identity);    // creates a cube in front of camera
+
+        audioSource.PlayOneShot(spawnClip, volume);//play audio clip
+
     }
     void OrderLadder()
     {
         int distFromCamera = 3;
         Instantiate(ladder, new Vector3(56,-20,150), Quaternion.identity);
+
+        audioSource.PlayOneShot(spawnClip, volume);//play audio clip
     }
     void OrderSlide()
     {
         int distFromCamera = 3;
         Instantiate(slide, new Vector3(0, -20, 150), Quaternion.identity);
+
+        audioSource.PlayOneShot(spawnClip, volume);//play audio clip
     }
     void OrderScrew()
     {
         int distFromCamera = 1;
         Instantiate(screw, new Vector3(48, -29, 101), Quaternion.identity);
+
+        audioSource.PlayOneShot(spawnClip, volume);//play audio clip
     }
 
     void ChangeSize()
