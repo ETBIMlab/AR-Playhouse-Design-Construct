bf0d1633fee6edbd3e6a293b9ef48ce970739b9e
Kenny Le <42878549+LeKenny7@users.noreply.github.com>
2021-02-01 16:34:00 -0700

    Drag object, dumpster, and eye tracking script

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class DragObject : MonoBehaviour
+{
+    public Camera cam;
+    public Transform thingy;
+    public float distanceFromCamera;
+    Rigidbody r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceFromCamera = Vector3.Distance(thingy.position, cam.transform.position);
+        r = thingy.GetComponent<Rigidbody>();
+    }
+    Vector3 lastPos;
+    // Update is called once per frame
+    void Update()
+    {
+        //if(IMixedRealityPointerHandler.OnPointerD)
+    }
+}
