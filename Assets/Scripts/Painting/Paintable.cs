dc29ba80580926fc7c5a22c6ab39dd7fc10993a8
Daucen <osbornd2017@gmail.com>
2021-03-08 00:38:01 -0700

    Make objects paintable and make paintbrush/paintbuckets orderable

--- Assets/Scripts/Painting/Paintable.cs
+++ Assets/Scripts/Painting/Paintable.cs
@@ -6,40 +6,40 @@ using UnityEngine.Windows.Speech;
 
 public class Paintable : MonoBehaviour
 {
-    KeywordRecognizer keywordRecognizer = null;
-    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    // KeywordRecognizer keywordRecognizer = null;
+    // Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
     // Start is called before the first frame update
-    void Start()
-    {
-        keywords.Add("Paint", () => { OpenMenu(); });
-        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
-        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
-        keywordRecognizer.Start();
-    }
+    // void Start()
+    // {
+        // keywords.Add("Paint", () => { OpenMenu(); });
+        // keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        // keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        // keywordRecognizer.Start();
+    // }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
 
-    }
+    // }
 
-    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
-    {
-        System.Action keywordAction;
-        if (keywords.TryGetValue(args.text, out keywordAction))
-        {
-            keywordAction.Invoke();
-        }
-    }
+    // private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    // {
+        // System.Action keywordAction;
+        // if (keywords.TryGetValue(args.text, out keywordAction))
+        // {
+            // keywordAction.Invoke();
+        // }
+    // }
 
-    public void OpenMenu()
-    {
-        Debug.Log("Opening Paint Menu");
-        GameObject paintMenu = GameObject.Find("PaintMenu");
-        paintMenu.SetActive(true);
-        paintMenu.GetComponent<Painter>().objectToBePainted = gameObject;
+    // public void OpenMenu()
+    // {
+       // Debug.Log("Opening Paint Menu");
+       // GameObject paintMenu = GameObject.Find("PaintMenu");
+       // paintMenu.SetActive(true);
+       // paintMenu.GetComponent<Painter>().objectToBePainted = gameObject;
 
-    }
+    // }
 
     public void ChangeColor(Material mat)
     {
