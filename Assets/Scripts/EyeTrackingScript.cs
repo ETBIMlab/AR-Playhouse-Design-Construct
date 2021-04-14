667e19fabfd094cba5a40449af7c405a221099b7
Kenny Le <42878549+LeKenny7@users.noreply.github.com>
2021-04-14 13:59:32 -0700

    Say export position logger to export positions

--- Assets/Scripts/EyeTrackingScript.cs
+++ Assets/Scripts/EyeTrackingScript.cs
@@ -2,17 +2,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class EyeTrackingScript : MonoBehaviour
 {
     public float capturePositionWaitTime = 1;
     float nextTime = 0;
+    string positions = "";
+
+    KeywordRecognizer keywordRecognizer = null;
+    List<string> keywords = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        keywords.Add("Export position logger");
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.ToArray());
+
+        // Register a callback for the KeywordRecognizer and START recognizing!!!!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
     }
 
     // Update is called once per frame
@@ -20,12 +34,21 @@ public class EyeTrackingScript : MonoBehaviour
     {
         if(Time.time >= nextTime)
         {
-            ActivityLogger logScript = GetComponent<ActivityLogger>();
+            //ActivityLogger logScript = GetComponent<ActivityLogger>();
+            positions += CoreServices.InputSystem.EyeGazeProvider.HitPosition.ToString() + "\n";
             //gameObject.GetComponent<ActivityLogger>().LogPosition(CoreServices.InputSystem.EyeGazeProvider.HitPosition.ToString());
-
-            //Debug.Log("Position: " + CoreServices.InputSystem.EyeGazeProvider.HitPosition);
             nextTime += capturePositionWaitTime;
-            //Debug.Log(CoreServices.InputSystem.EyeGazeProvider.HitInfo.)
         }
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        ExportPositionLog();
+    }
+
+    void ExportPositionLog()
+    {
+        Debug.Log("Printing Position Log");
+        File.WriteAllText("./PositionLog.txt", positions);
     }
 }
--- Assets/Scripts/KennyLocking.cs
+++ Assets/Scripts/KennyLocking.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 using System.Linq;
+using System;
 
 public class KennyLocking : MonoBehaviour
 {
@@ -28,7 +29,7 @@ public class KennyLocking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
