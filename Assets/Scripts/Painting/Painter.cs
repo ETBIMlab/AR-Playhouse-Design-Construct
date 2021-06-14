2b134401c8990b38a7ae50540ddf569f70b06118
totaltrashmamal <42754922+totaltrashmamal@users.noreply.github.com>
2021-06-13 20:42:05 -0700

    paintbrush assigns new material not new color.

--- Assets/Scripts/Painting/Painter.cs
+++ Assets/Scripts/Painting/Painter.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class Painter : MonoBehaviour
 {
@@ -9,15 +17,33 @@ public class Painter : MonoBehaviour
     public GameObject AssignToBrush;
     public bool colorPicked = false;
     private bool toolGrabbed = false;
-
-    public Renderer ren;
+    public Color currentColor;
+    public Color objCurrentColor;
     public Material[] mat;
+    public Renderer ren;
 
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
     // Start is called before the first frame update
-    void Start()
+   /* void Start()
     {
+        keywords.Add("Paint surface Red", () => { this.BroadcastMessage("Red"); });
+        keywords.Add("Paint surface Orange", () => { this.BroadcastMessage("Orange"); });
+        keywords.Add("Paint surface Yellow", () => { this.BroadcastMessage("Yellow"); });
+        keywords.Add("Paint surface Green", () => { this.BroadcastMessage("Green"); });
+        keywords.Add("Paint surface Blue", () => { this.BroadcastMessage("Blue"); });
+        keywords.Add("Paint surface Indigo", () => { this.BroadcastMessage("Indigo"); });
+        keywords.Add("Paint surface Purple", () => { this.BroadcastMessage("Purple"); });
 
-    }
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and START recognizing!!!!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+         
+    }*/
 
     public void OnGrab()
     {
@@ -30,7 +56,7 @@ public class Painter : MonoBehaviour
         Debug.Log("Painter trying to paint");
         if (objectToBePainted != null && objectToBePainted.GetComponent<Paintable>() != null && toolGrabbed == true)
         {
-            objectToBePainted.GetComponent<Paintable>().ChangeColor(material);
+            //objectToBePainted.GetComponent<Painter>().KeywordRecognizer_OnPhraseRecognized();
         }
         else if (objectToBePainted == null)
         {
@@ -66,8 +92,48 @@ public class Painter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    /* private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+     {
+         System.Action keywordAction;
 
-    }
+         if (objCurrentColor != currentColor)
+         {
+             if (args.text.Substring(18) == "Red")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", Color.red);
+
+             }
+             if (args.text.Substring(18) == "Orange")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", (255, 165, 0));
+
+             }
+             if (args.text.Substring(18) == "Yellow")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", Color.yellow);
+
+             }
+             if (args.text.Substring(18) == "Green")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", Color.green);
+
+             }
+             if (args.text.Substring(18) == "Blue")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", Color.blue);
+
+             }
+             if (args.text.Substring(18) == "Indigo")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", (75, 0, 130));
+
+             }
+             if (args.text.Substring(18) == "Violet")
+             {
+                 ren.AssignToBrush.SetColor = ("_Color", (238, 130, 238));
+
+             }
+
+         }
+     }*/
 }
