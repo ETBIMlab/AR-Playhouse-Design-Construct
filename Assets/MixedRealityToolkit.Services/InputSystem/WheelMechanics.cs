02efcaf77f75b2757fc6bd2d2cab0c759f71aa67
Ileecea Askew <72593006+IleeceaA@users.noreply.github.com>
2021-06-22 14:55:38 -0500

    Wheel Test
    Added Wheel Test Scene and Wheel Mechanics

---
+++ Assets/MixedRealityToolkit.Services/InputSystem/WheelMechanics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMechanics : MonoBehaviour
+{
+    float rotSpeed = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        transform.Rotate(0, 0, rotSpeed);
+        this.rotSpeed *= 0.96f;
+    }
+}
