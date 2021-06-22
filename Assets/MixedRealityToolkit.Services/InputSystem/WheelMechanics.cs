6a3386428dac006fa71b88234840ed370bbe1b21
Ileecea Askew <72593006+IleeceaA@users.noreply.github.com>
2021-06-22 15:01:59 -0500

    Update WheelMechanics.cs

--- Assets/MixedRealityToolkit.Services/InputSystem/WheelMechanics.cs
+++ Assets/MixedRealityToolkit.Services/InputSystem/WheelMechanics.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WheelMechanics : MonoBehaviour
 {
+    //This represents rotational speed
     float rotSpeed = 0; 
 
     // Start is called before the first frame update
@@ -15,12 +16,14 @@ public class WheelMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Once selected the wheel should spin
         if(Input.GetMouseButtonDown(0))
         {
             this.rotSpeed = 10;
         }
-
         transform.Rotate(0, 0, rotSpeed);
+
+        //Added for the speed to slow down
         this.rotSpeed *= 0.96f;
     }
 }
