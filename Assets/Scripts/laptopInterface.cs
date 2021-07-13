bdf7e21af08234c898717e124815af51ddda1c63
Ileecea Askew <72593006+IleeceaA@users.noreply.github.com>
2021-07-13 13:23:53 -0500

    updates

--- Assets/Scripts/laptopInterface.cs
+++ Assets/Scripts/laptopInterface.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine.Windows.Speech;
 
