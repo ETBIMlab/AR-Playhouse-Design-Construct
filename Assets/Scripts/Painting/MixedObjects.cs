bda7c358ecfe6b402d76fabc38cd5c5b4bde7a01
totaltrashmamal <42754922+totaltrashmamal@users.noreply.github.com>
2021-07-25 14:13:54 -0700

    don't want to lose progress :)

--- Assets/Scripts/Painting/MixedObjects.cs
+++ Assets/Scripts/Painting/MixedObjects.cs
@@ -15,17 +15,17 @@ public class MixedObjects : MonoBehaviour
     /// </summary>
     //Tube Bridge
     public GameObject AssignToYellowTubeBridge;
-    public GameObject AssignToRedTubeBridge;
+    public GameObject AssignToRedTubeBridge; //check
     public GameObject AssignToGreenTubeBridge;
     public GameObject AssignToBlueTubeBridge;
     //Small Chimes: Chimes Panel 5' by 3'6in 
     public GameObject AssignToYellowSmallChimes;
-    public GameObject AssignToRedSmallChimes;
+    public GameObject AssignToRedSmallChimes; // :)
     public GameObject AssignToGreenSmallChimes;
     public GameObject AssignToBlueSmallChimes;
     //Large Chimes: Chimes Panel 5' by 5'4in
     public GameObject AssignToYellowBigChimes;
-    public GameObject AssignToRedBigChimes;
+    public GameObject AssignToRedBigChimes; //:)
     public GameObject AssignToGreenBigChimes;
     public GameObject AssignToBlueBigChimes;
     //Single Slide: 
@@ -63,47 +63,47 @@ public class MixedObjects : MonoBehaviour
     void Start()
     {
        //red
-        keywords.Add("Order Chimes Panel 5' by 3'6in in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Chimes Panel 5' by 5'4in in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Tube Bridge in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Single Slide in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Double Slide in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Tube Slide in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Firepole in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Coil Climber in Red", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Stairs in Red", () => { this.BroadcastMessage("Red"); });
+        keywords.Add("Order Chimes Panel 5' by 3'6in in Red", () => { this.BroadcastMessage("SmallChimesRed"); });
+        keywords.Add("Order Chimes Panel 5' by 5'4in in Red", () => { this.BroadcastMessage("BigChimesRed"); });
+        keywords.Add("Order Tube Bridge in Red", () => { this.BroadcastMessage("TubeBridgeRed"); });
+        keywords.Add("Order Single Slide in Red", () => { this.BroadcastMessage("SingleSlideRed"); });
+        keywords.Add("Order Double Slide in Red", () => { this.BroadcastMessage("DoubleSlideRed"); });
+        //keywords.Add("Order Tube Slide in Red", () => { this.BroadcastMessage("TubeSlideRed"); });
+        keywords.Add("Order Firepole in Red", () => { this.BroadcastMessage("FirepoleRed"); });
+        keywords.Add("Order Coil Climber in Red", () => { this.BroadcastMessage("CoilClimberRed"); });
+        keywords.Add("Order Stairs in Red", () => { this.BroadcastMessage("StairsRed"); });
 
         //yellow
-        keywords.Add("Order Chimes Panel 5' by 3'6in", () => { this.BroadcastMessage("Yellow"); });
-        keywords.Add("Order Chimes Panel 5' by 5'4in", () => { this.BroadcastMessage("Yellow"); });
-        keywords.Add("Order Tube Bridge", () => { this.BroadcastMessage("Yellow"); });
-        keywords.Add("Order Single Slide", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Double Slide", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Tube Slide", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Firepole", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Coil Climber", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Stairs", () => { this.BroadcastMessage("Red"); });
+        keywords.Add("Order Chimes Panel 5' by 3'6in", () => { this.BroadcastMessage("SmallChimesYellow"); });
+        keywords.Add("Order Chimes Panel 5' by 5'4in", () => { this.BroadcastMessage("BigChimesYellow"); });
+        keywords.Add("Order Tube Bridge", () => { this.BroadcastMessage("TubeBridgeYellow"); });
+        keywords.Add("Order Single Slide", () => { this.BroadcastMessage("SingleSlideYellow"); });
+        keywords.Add("Order Double Slide", () => { this.BroadcastMessage("DoubleSlideYellow"); });
+        //keywords.Add("Order Tube Slide", () => { this.BroadcastMessage("TubeSlideYellow"); });
+        keywords.Add("Order Firepole", () => { this.BroadcastMessage("FirepoleYellow"); });
+        keywords.Add("Order Coil Climber", () => { this.BroadcastMessage("CoilClimberYellow"); });
+        keywords.Add("Order Stairs", () => { this.BroadcastMessage("StairsYellow"); });
         //green
-        keywords.Add("Order Chimes Panel 5' by 3'6in in Green", () => { this.BroadcastMessage("Green"); });
-        keywords.Add("Order Chimes Panel 5' by 5'4in in Green", () => { this.BroadcastMessage("Green"); });
-        keywords.Add("Order Tube Bridge in Green", () => { this.BroadcastMessage("Green"); });
-        keywords.Add("Order Single Slide in Green", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Double Slide in Green", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Tube Slide in Green", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Firepole in Green", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Coil Climber in Green", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Stairs in Green", () => { this.BroadcastMessage("Red"); });
+        keywords.Add("Order Chimes Panel 5' by 3'6in in Green", () => { this.BroadcastMessage("SmallChimesGreen"); });
+        keywords.Add("Order Chimes Panel 5' by 5'4in in Green", () => { this.BroadcastMessage("BigChimesGreen"); });
+        keywords.Add("Order Tube Bridge in Green", () => { this.BroadcastMessage("TubeBridgeGreen"); });
+        keywords.Add("Order Single Slide in Green", () => { this.BroadcastMessage("SingleSlideGreen"); });
+        keywords.Add("Order Double Slide in Green", () => { this.BroadcastMessage("DoubleSlideGreen"); });
+        //keywords.Add("Order Tube Slide in Green", () => { this.BroadcastMessage("TubeSlideGreen"); });
+        keywords.Add("Order Firepole in Green", () => { this.BroadcastMessage("FirepoleGreen"); });
+        keywords.Add("Order Coil Climber in Green", () => { this.BroadcastMessage("CoilClimberGreen"); });
+        keywords.Add("Order Stairs in Green", () => { this.BroadcastMessage("StairsGreen"); });
         //Blue
-        keywords.Add("Order Chimes Panel 5' by 3'6in in Blue", () => { this.BroadcastMessage("Blue"); });
-        keywords.Add("Order Chimes Panel 5' by 5'4in in Blue", () => { this.BroadcastMessage("Blue"); });
-        keywords.Add("Order Tube Bridge in Blue", () => { this.BroadcastMessage("Blue"); });
-        keywords.Add("Order Single Slide in Blue", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Double Slide in Blue", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Tube Slide in Blue", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Firepole in Blue", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Coil Climber in Blue", () => { this.BroadcastMessage("Red"); });
-        keywords.Add("Order Stairs in Blue", () => { this.BroadcastMessage("Red"); });
-
+        keywords.Add("Order Chimes Panel 5' by 3'6in in Blue", () => { this.BroadcastMessage("SmallChimesBlue"); });
+        keywords.Add("Order Chimes Panel 5' by 5'4in in Blue", () => { this.BroadcastMessage("BigChimesBlue"); });
+        keywords.Add("Order Tube Bridge in Blue", () => { this.BroadcastMessage("TubeBridgeBlue"); });
+        keywords.Add("Order Single Slide in Blue", () => { this.BroadcastMessage("SingleSlideBlue"); });
+        keywords.Add("Order Double Slide in Blue", () => { this.BroadcastMessage("DoubleSlideBlue"); });
+       // keywords.Add("Order Tube Slide in Blue", () => { this.BroadcastMessage("TubeSlideBlue"); });
+        keywords.Add("Order Firepole in Blue", () => { this.BroadcastMessage("FirepoleBlue"); });
+        keywords.Add("Order Coil Climber in Blue", () => { this.BroadcastMessage("CoilClimberBlue"); });
+        keywords.Add("Order Stairs in Blue", () => { this.BroadcastMessage("StairsBlue"); });
+        
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
@@ -115,74 +115,149 @@ public class MixedObjects : MonoBehaviour
     {
         Action keywordAction;
         //change the color based on the color we want
-        if (args.text.Substring(32) == "Red")
+        //red
+        if (args.text.Substring(32) == "SmallChimesRed")
         {
-            if(objectOrdered.GetComponent<IsWoodOrPlastic>().isTubeBridge == true)
-            {
-                objectOrdered = AssignToRedTubeBridge;
-            }
-            else if(objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesSmall == true)
-            {
-                objectOrdered = AssignToRedSmallChimes;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesLarge == true)
-            {
-                objectOrdered = AssignToRedBigChimes;
-            }
-            Debug.Log("Color changed to red");
-
+            objectOrdered = AssignToRedSmallChimes;
+            Debug.Log("Red Small Chimes Ordered!");
         }
-      
-        else if (args.text.Substring(32) == "Yellow")
+        else if (args.text.Substring(32) == "BigChimesRed")
         {
-            if (objectOrdered.GetComponent<IsWoodOrPlastic>().isTubeBridge == true)
-            {
-                objectOrdered = AssignToYellowTubeBridge;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesSmall == true)
-            {
-                objectOrdered = AssignToYellowSmallChimes;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesLarge == true)
-            {
-                objectOrdered = AssignToYellowBigChimes;
-            }
-            Debug.Log("Ordered default yellow");
-
+            objectOrdered = AssignToRedBigChimes;
+            Debug.Log("Red Big Chimes Ordered!");
         }
-        else if (args.text.Substring(32) == "Green")
+        else if (args.text.Substring(32) == "TubeBridgeRed")
         {
-            if (objectOrdered.GetComponent<IsWoodOrPlastic>().isTubeBridge == true)
-            {
-                objectOrdered = AssignToGreenTubeBridge;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesSmall == true)
-            {
-                objectOrdered = AssignToGreenSmallChimes;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesLarge == true)
-            {
-                objectOrdered = AssignToGreenBigChimes;
-            }
-            Debug.Log("Color changed to green");
-
+            objectOrdered = AssignToRedTubeBridge;
+            Debug.Log("Red Tube Bridge Ordered!");
         }
-        else if (args.text.Substring(32) == "Blue")
+        else if (args.text.Substring(32) == "SingleSlideRed")
         {
-            if (objectOrdered.GetComponent<IsWoodOrPlastic>().isTubeBridge == true)
-            {
-                objectOrdered = AssignToBlueTubeBridge;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesSmall == true)
-            {
-                objectOrdered = AssignToBlueSmallChimes;
-            }
-            else if (objectOrdered.GetComponent<IsWoodOrPlastic>().isChimesLarge == true)
-            {
-                objectOrdered = AssignToBlueBigChimes;
-            }
-            Debug.Log("Color changed to blue");
-
+            objectOrdered = AssignToRedSingleSlide;
+            Debug.Log("Red Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "DoubleSlideRed")
+        {
+            objectOrdered = AssignToRedDoubleSlide;
+            Debug.Log("Red Double Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "FirepoleRed")
+        {
+            objectOrdered = AssignToRedFirepole;
+            Debug.Log("Red Firepole Ordered!");
+        }
+        else if (args.text.Substring(32) == "CoilClimberRed")
+        {
+            objectOrdered = AssignToRedCoilClimber;
+            Debug.Log("Red Coil Climber Ordered!");
+        }
+        //yellow
+        if (args.text.Substring(32) == "SmallChimesYellow")
+        {
+            objectOrdered = AssignToYellowSmallChimes;
+            Debug.Log("Yellow Small Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "BigChimesYellow")
+        {
+            objectOrdered = AssignToYellowBigChimes;
+            Debug.Log("Yellow Big Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "TubeBridgeYellow")
+        {
+            objectOrdered = AssignToYellowTubeBridge;
+            Debug.Log("Yellow Tube Bridge Ordered!");
+        }
+        else if (args.text.Substring(32) == "SingleSlideYellow")
+        {
+            objectOrdered = AssignToYellowSingleSlide;
+            Debug.Log("Yellow Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "DoubleSlideYellow")
+        {
+            objectOrdered = AssignToYellowDoubleSlide;
+            Debug.Log("Yellow Double Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "FirepoleYellow")
+        {
+            objectOrdered = AssignToYellowFirepole;
+            Debug.Log("Yellow Firepole Ordered!");
+        }
+        else if (args.text.Substring(32) == "CoilClimberYellow")
+        {
+            objectOrdered = AssignToYellowCoilClimber;
+            Debug.Log("Yellow Coil Climber Ordered!");
+        }
+        //green
+        if (args.text.Substring(32) == "SmallChimesGreen")
+        {
+            objectOrdered = AssignToGreenSmallChimes;
+            Debug.Log("Green Small Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "BigChimesGreen")
+        {
+            objectOrdered = AssignToGreenBigChimes;
+            Debug.Log("Green Big Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "TubeBridgeGreen")
+        {
+            objectOrdered = AssignToGreenTubeBridge;
+            Debug.Log("Green Tube Bridge Ordered!");
+        }
+        else if (args.text.Substring(32) == "SingleSlideGreen")
+        {
+            objectOrdered = AssignToGreenSingleSlide;
+            Debug.Log("Green Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "DoubleSlideGreen")
+        {
+            objectOrdered = AssignToGreenDoubleSlide;
+            Debug.Log("Green Double Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "FirepoleGreen")
+        {
+            objectOrdered = AssignToGreenFirepole;
+            Debug.Log("Green Firepole Ordered!");
+        }
+        else if (args.text.Substring(32) == "CoilClimberGreen")
+        {
+            objectOrdered = AssignToGreenCoilClimber;
+            Debug.Log("Green Coil Climber Ordered!");
+        }
+        //blue
+        if (args.text.Substring(32) == "SmallChimesBlue")
+        {
+            objectOrdered = AssignToBlueSmallChimes;
+            Debug.Log("Blue Small Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "BigChimesBlue")
+        {
+            objectOrdered = AssignToBlueBigChimes;
+            Debug.Log("Blue Big Chimes Ordered!");
+        }
+        else if (args.text.Substring(32) == "TubeBridgeBlue")
+        {
+            objectOrdered = AssignToBlueTubeBridge;
+            Debug.Log("Blue Tube Bridge Ordered!");
+        }
+        else if (args.text.Substring(32) == "SingleSlideBlue")
+        {
+            objectOrdered = AssignToBlueSingleSlide;
+            Debug.Log("Blue Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "DoubleSlideBlue")
+        {
+            objectOrdered = AssignToBlueDoubleSlide;
+            Debug.Log("Blue Double Slide Ordered!");
+        }
+        else if (args.text.Substring(32) == "FirepoleBlue")
+        {
+            objectOrdered = AssignToBlueFirepole;
+            Debug.Log("Blue Firepole Ordered!");
+        }
+        else if (args.text.Substring(32) == "CoilClimberBlue")
+        {
+            objectOrdered = AssignToBlueCoilClimber;
+            Debug.Log("Blue Coil Climber Ordered!");
         }
         //the following is from ObjectOrderer, from my understanding it loads the object into the truck
         var orderPos = GameObject.Find("IndustrialSmallTruck").transform.position;
