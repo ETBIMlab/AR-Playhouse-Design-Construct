81120b03eeb2aa94934e318119f92124892d8f5c
totaltrashmamal <42754922+totaltrashmamal@users.noreply.github.com>
2021-08-01 16:09:05 -0700

    painter updates

--- Assets/Scripts/Painting/Painter.cs
+++ Assets/Scripts/Painting/Painter.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor.SceneManagement;
 #endif
-using UnityEngine;
-using UnityEngine.Windows.Speech;
 
 public class Painter : MonoBehaviour
 {
@@ -21,7 +18,10 @@ public class Painter : MonoBehaviour
     //public Color newColor; //color we want to paint it
     //public Color objCurrentColor; //before painting
     public MeshRenderer ren;
+    public MeshRenderer pren;
+
     public Material[] mat;
+    public Material[] pmat;
     //public Renderer ren; //renderer for paint brush - new color IGNORE THIS
     //public Renderer objectRen; //renderer for object so we  can change the color IGNORE
 
@@ -54,9 +54,11 @@ public class Painter : MonoBehaviour
             {
                 Debug.Log("I collided with a non paint bucket!");
 
-                //other.GetComponent<MeshRenderer>().material = material; //we are leaving the thing alone
                 //we are going to set the color as the color of the paint which is on the brush
-                other.GetComponent<MeshRenderer>().material = newColor;
+                mat = other.GetComponent<MeshRenderer>().materials;
+                mat[0] = newColor;
+                Debug.Log("I gave it a new color");
+
             }
             //the below is if we come into contact with a paint bucket, the above is if we come into contact with
             //another object (wood for example)
@@ -65,11 +67,12 @@ public class Painter : MonoBehaviour
                 Debug.Log("Collided with paint bucket!");
 
                 colorPicked = true;//yes we picked a color
-                                   //I don't know how this works so we comment out and pray it doesn't break anything
-                newColor = other.GetComponent<MeshRenderer>().material; //we want the object to keep it's current material, we just want to change the color
-                   mat = ren.materials;
-                   mat[3] = newColor;
-                  AssignToBrush.GetComponent<MeshRenderer>().materials = mat;
+               //we want the object to keep it's current material, we just want to change the color
+                mat = other.GetComponent<MeshRenderer>().materials;
+                mat[2] = newColor;
+                Debug.Log("Grabbed new color");        
+                pmat = AssignToBrush.GetComponent<MeshRenderer>().materials;
+                pmat[3] = newColor;
                 Debug.Log("Assigned Brush the new color!");
             }
         }
